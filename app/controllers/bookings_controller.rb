7e51acd7ae7dbe272664c4eccbf13d45ab75461e
Ikraam <ikraamghoor@gmail.com>
2020-10-26 09:30:11 +0200

    Refactor booking sorting algorithm

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,80 +8,20 @@ class BookingsController < ApplicationController
 
   def index
     @booking = Booking.new
-
-    # @bookings = Booking.order(timeslot: :asc, location: :asc, destination: :asc)
-    @bookings =  Booking.all
+    @bookings = Booking.all
 
     @home_location = '64 Rigger Rd, Spartan, Kempton Park, 1619'
-    @bookings_inbound = @bookings.where('location = ?', @home_location)
-    @bookings_outbound = @bookings.where('destination = ?', @home_location)
-
-    # @other_bookings = Booking.where.not('location = ? AND destination = ?', @home_location, @home_location)
-
-    @grouped_outbound_bookings = @bookings_outbound.group_by(&:timeslot)
-    @grouped_inbound_bookings = @bookings_inbound.group_by(&:timeslot)
-
-    @trips = []
-    ### Create inbound trips
-    @grouped_inbound_bookings.each do |_time, time_group|
-      remaining_bookings = time_group.clone
-      ### Loop if more than one booking in timeslot
-      while remaining_bookings.count > 1
-        ### Check furthest location
-        furthest_booking = get_furthest_booking(remaining_bookings)
-        remaining_bookings = remaining_bookings.reject { |booking| booking == furthest_booking }
-        ### Find closest location to furthest
-        closest = 999_999_999
-        next_booking = nil
-        remaining_bookings.each do |booking|
-          distance = furthest_booking.deslonlat.distance(booking.deslonlat)
-          if distance < closest
-            closest = distance
-            next_booking = booking
-          end
-        end
-        ### Add single or double bookings to trips
-        if closest > 15_000
-          @trips << [furthest_booking]
-        else
-          @trips << [furthest_booking, next_booking]
-          remaining_bookings = remaining_bookings.reject { |booking| booking == next_booking }
-        end
-      end
-      ### Add remaining booking to trip
-      @trips << remaining_bookings if remaining_bookings.count == 1
-    end
-
-    ### Create outbound trips
-    @grouped_outbound_bookings.each do |_time, time_group|
-      remaining_bookings = time_group.clone
-      ### Loop if more than one booking in timeslot
-      while remaining_bookings.count > 1
-        ### Check furthest location
-        furthest_booking = get_furthest_booking(remaining_bookings)
-        remaining_bookings = remaining_bookings.reject { |booking| booking == furthest_booking }
-        ### Find closest location to furthest
-        closest = 999_999_999
-        next_booking = nil
-        remaining_bookings.each do |booking|
-          distance = furthest_booking.loclonlat.distance(booking.loclonlat)
-          if distance < closest
-            closest = distance
-            next_booking = booking
-          end
-        end
-        ### Add single or double bookings to trips
-        if closest > 15_000
-          @trips << [furthest_booking]
-        else
-          @trips << [furthest_booking, next_booking]
-          remaining_bookings = remaining_bookings.reject { |booking| booking == next_booking }
-        end
-      end
-      ### Add remaining booking to trip
-      @trips << remaining_bookings if remaining_bookings.count == 1
-    end
+    bookings_inbound = @bookings.where('location = ?', @home_location).group_by(&:timeslot)
+    bookings_outbound = @bookings.where('destination = ?', @home_location).group_by(&:timeslot)
+    # other_bookings = @bookings.where.not('location = ? AND destination = ?', @home_location, @home_location)
+    @trips = get_trips(bookings_inbound, true)
+    @trips.concat(get_trips(bookings_outbound, false))
     @trips = @trips.sort_by { |trip| trip[0].timeslot }
+
+    respond_to do |format|
+      format.json { render json: @trips, status: :ok }
+      format.html
+    end
   end
 
   # GET /bookings/1
@@ -144,10 +84,20 @@ class BookingsController < ApplicationController
 
   private
 
-  def get_furthest_booking(remaining_bookings)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def booking_params
+    params.require(:booking).permit(:passenger, :location, :destination, :timeslot, :file)
+  end
+
+  def get_furthest_booking(bookings)
     furthest = 0
     furthest_booking = nil
-    remaining_bookings.each do |booking|
+    bookings.each do |booking|
       distance = booking.loclonlat.distance(booking.deslonlat)
       if distance > furthest
         furthest = distance
@@ -157,13 +107,41 @@ class BookingsController < ApplicationController
     furthest_booking
   end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_booking
-    @booking = Booking.find(params[:id])
-  end
+  def get_trips(booking_groups, inbound)
+    trips = []
+    booking_groups.each do |_time, remaining_bookings|
+      ### Loop if more than one booking in timeslot
+      while remaining_bookings.count > 1
+        ### Check furthest location and extract it
+        furthest_booking = get_furthest_booking(remaining_bookings)
+        remaining_bookings = remaining_bookings.reject { |booking| booking == furthest_booking }
+        ### Find closest location to furthest
+        closest = 999_999_999
+        next_booking = nil
+        remaining_bookings.each do |booking|
+          ### Change distance calculation upon direction
+          distance = if inbound
+                       furthest_booking.deslonlat.distance(booking.deslonlat)
+                     else
+                       furthest_booking.loclonlat.distance(booking.loclonlat)
+                     end
 
-  # Only allow a list of trusted parameters through.
-  def booking_params
-    params.require(:booking).permit(:passenger, :location, :destination, :timeslot, :file)
+          if distance < closest
+            closest = distance
+            next_booking = booking
+          end
+        end
+        ### Add single or double bookings to trips
+        if closest > 15_000
+          trips << [furthest_booking]
+        else
+          trips << [furthest_booking, next_booking]
+          remaining_bookings = remaining_bookings.reject { |booking| booking == next_booking }
+        end
+      end
+      ### Add remaining booking to trip
+      trips << remaining_bookings if remaining_bookings.count == 1
+    end
+    trips
   end
 end
