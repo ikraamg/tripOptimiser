13e828a38aad56be2b669a82cc966aef846b4cc6
Ikraam <ikraamghoor@gmail.com>
2020-10-26 19:41:27 +0200

    Default root route to bookings#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   resources :bookings
   post 'bookings/upload', to: 'bookings#csv_create'
+  root 'bookings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
